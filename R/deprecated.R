211d42f7b686472bd8ac3570b3bff9b2ae0c3f56
Colin <colin@thinkr.fr>
2019-01-06 00:09:20 +0100

    deprecated functions Rd

--- R/deprecated.R
+++ R/deprecated.R
@@ -1,13 +1,19 @@
-#' @export
-#' @noRd
+#' Deprecated functions from \pkg{rpinterest}.
 #' 
+#' @name rpinterest-deprecated
+#' @keywords internal
+NULL
+  
+#' @rdname rpinterest-deprecated
+#' @export
+
 BoardPinsByID <- function(boardID, token) {
   .Deprecated("get_board_pins_by_id", msg = 'BoardPinsByID is now deprecated, please use get_board_pins_by_id()')
   get_board_pins_by_id(boardID, token)
 }
 
+#' @rdname rpinterest-deprecated
 #' @export
-#' @noRd
 #' 
 BoardPinsByName <- function(user, board, token) {
   .Deprecated("get_board_pins_by_name", msg = 'BoardPinsByName is now deprecated, please use get_board_pins_by_name()')
@@ -15,41 +21,41 @@ BoardPinsByName <- function(user, board, token) {
 }
 
 
+#' @rdname rpinterest-deprecated
 #' @export
-#' @noRd
 #' 
 BoardSpecByID <- function(boardID, token) {
   .Deprecated("get_board_spec_by_id", msg = 'BoardSpecByID is now deprecated, please use get_board_spec_by_id()')
   get_board_spec_by_id(boardID, token)
 }
 
+#' @rdname rpinterest-deprecated
 #' @export
-#' @noRd
-#' 
+#'  
 BoardSpecByName <- function(user, board, token){
   .Deprecated("get_board_spec_by_name", msg = 'BoardSpecByName is now deprecated, please use get_board_spec_by_id()')
   get_board_spec_by_name(user, board, token)
 }
 
+#' @rdname rpinterest-deprecated
 #' @export
-#' @noRd
 #' 
 PinSpecByID <- function(pinID, token){
   .Deprecated("get_pin_spec_by_id", msg = 'PinSpecByID is now deprecated, please use get_pin_spec_by_id()')
   get_pin_spec_by_id(pinID, token)
 }
 
+#' @rdname rpinterest-deprecated
 #' @export
-#' @noRd
 #' 
 UserSpecByID <- function(userID, token){
   .Deprecated("get_user_spec_by_id", msg = 'UserSpecByID is now deprecated, please use get_user_spec_by_id()')
   get_user_spec_by_id(userID, token)
 }
 
+#' @rdname rpinterest-deprecated
 #' @export
-#' @noRd
-#' 
+#'  
 UserSpecByName <- function(user, token){
   .Deprecated("get_user_spec_by_name", msg = 'UserSpecByName is now deprecated, please use get_user_spec_by_name()')
   get_user_spec_by_name(user, token)
