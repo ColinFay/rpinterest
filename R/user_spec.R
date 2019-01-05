7ff3210a83414b6cdb1c856c611dec74cfef7b98
Colin <colin@thinkr.fr>
2019-01-05 23:40:25 +0100

    fixed user_spec

--- R/user_spec.R
+++ R/user_spec.R
@@ -3,7 +3,7 @@
 #'Get information about a pinterest user using the user ID.
 #'
 #'Takes a user ID and an access token, returns a data.frame.
-#'@param id a character string with an user ID.
+#'@param id a character string with a user ID.
 #'@inheritParams get_board_pins_by_id
 #'
 #'@return Always returns a data.frame, with a warning when appropriate.
@@ -25,7 +25,6 @@ get_user_spec_by_id <- function(id, token){
 #'
 #'Takes a user name, a board name and an access token, returns a data.frame.
 #'@param user a character string with a user name.
-#'@param board a character string with a board name.
 #'@inheritParams get_board_pins_by_id
 #'
 #'@return Always returns a data.frame, with a warning when appropriate.
@@ -33,36 +32,32 @@ get_user_spec_by_id <- function(id, token){
 #'@examples
 #'get_user_spec_by_name(user = "colinfay", token = token)
 
-get_user_spec_by_name <- function(name, token){
+get_user_spec_by_name <- function(user, token){
   url <- paste0("https://api.pinterest.com/v1/users/", user, "/?access_token=", token, "&fields=first_name%2Cid%2Clast_name%2Curl%2Caccount_type%2Cusername%2Cbio%2Ccounts%2Ccreated_at%2Cimage")
   get_user_spec(url)
 }
 
 
 get_user_spec <- function(url){
+  browser()
   check_internet()
   res <- httr::GET(url)
   check_status(res)
-  content <- json_raw_to_char(board$content)
-  content <- content$data
-  contentlist <- list()
-  contentlist[[1]] <- content
-  identity <- lapply(contentlist, function(obj) {
-    data.frame(id = obj$id %||% NA, 
-               names = obj$username %||% NA,
-               bio = obj$bio %||% NA, 
-               first_name = obj$first_name %||% NA, 
-               last_name = obj$last_name %||% NA, 
-               account_type = obj$account_type %||% NA,
-               url = obj$url %||% NA,
-               created_at = obj$created_at %||% NA,
-               pins = obj$counts$pins %||% NA,
-               following = obj$counts$following %||% NA,
-               followers = obj$counts$followers %||% NA,
-               boards = obj$counts$boards %||% NA,
-               likes = obj$counts$likes %||% NA, 
-               stringsAsFactors = FALSE)
-  }) 
-  do.call(rbind, identity)
-
+  content <- json_raw_to_char(res$content)$data
+  
+  data.frame(id = content$id %||% NA, 
+             names = content$username %||% NA,
+             bio = content$bio %||% NA, 
+             first_name = content$first_name %||% NA, 
+             last_name = content$last_name %||% NA, 
+             account_type = content$account_type %||% NA,
+             url = content$url %||% NA,
+             created_at = content$created_at %||% NA,
+             pins = content$counts$pins %||% NA,
+             following = content$counts$following %||% NA,
+             followers = content$counts$followers %||% NA,
+             boards = content$counts$boards %||% NA,
+             likes = content$counts$likes %||% NA, 
+             stringsAsFactors = FALSE)
+  
 }
