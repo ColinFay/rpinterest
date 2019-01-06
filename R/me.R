21ab9c9c086efa16fea14f58cf1342e8656b95bc
Colin <colin@thinkr.fr>
2019-01-06 22:27:14 +0100

    logged user funs

--- R/me.R
+++ R/me.R
@@ -4,17 +4,20 @@
 #' their boards.
 #'
 #'@inheritParams get_board_pins_by_id
+#' @param count for suggestions, the max number of suggestion to retrieve.
 #'
 #' @rdname get_logged_user
 #'
-#' @return a data.frame
+#'@return Either a data.frame, or an error.
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' get_logged_user(token)
 #' get_logged_user_boards(token)
+#' get_logged_user_boards_suggestions(token = token)
 #' }
+
 get_logged_user <- function(token){
   check_internet()
   res <- httr::GET(
@@ -28,6 +31,7 @@ get_logged_user <- function(token){
 
 #' @rdname get_logged_user
 #' @export
+
 get_logged_user_boards <- function(token){
   check_internet()
   res <- httr::GET(
@@ -37,3 +41,28 @@ get_logged_user_boards <- function(token){
   check_status(res)
   as_tbl(json_raw_to_char(res$content)$data)
 }
+
+#' @rdname get_logged_user
+#' @export
+
+get_logged_user_boards_suggestions <- function(count = 25, token){
+  check_internet()
+  res <- httr::GET(
+    paste0("https://api.pinterest.com/v1/me/boards/suggested/?count=", count, "&access_token=", token, "&fields=id%2Cname%2Curl%2Ccounts%2Ccreated_at%2Ccreator%2Cdescription%2Cimage%2Cprivacy%2Creason")
+  )
+  check_status(res)
+  as_tbl(json_raw_to_char(res$content)$data)
+}
+
+
+#' #' @rdname get_logged_user
+#' #' @export
+#' get_logged_user_followers <- function(token){
+#'   check_internet()
+#'   res <- httr::GET(
+#'     paste0("https://api.pinterest.com/v1/me/followers/?access_token=", token,"&fields=first_name%2Cid%2Clast_name%2Curl%2Caccount_type%2Cbio%2Ccounts%2Ccreated_at%2Cimage%2Cusername")
+#'   )
+#'   check_status(res)
+#'   as_tbl(json_raw_to_char(res$content)$data)
+#' }
+
